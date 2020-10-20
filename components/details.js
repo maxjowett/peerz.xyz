fc4dd48b8648fd36e22180e8d664de8bd90846b6
Max Jowett <jowett.max@gmail.com>
2020-10-19 22:31:21 -0700

    Add details component

---
+++ components/details.js
@@ -0,0 +1,31 @@
+import styles from '../styles/Details.module.scss';
+
+const Details = () => {
+  return (
+    <div className="g__row">
+      <div className={styles.details}>
+        <div>
+          <h4>Ready to give it a shot?</h4>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
