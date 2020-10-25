a82909aa93dbcc0770f68e906c921e680035d995
Max Jowett <jowett.max@gmail.com>
2020-10-24 20:32:34 -0700

    Nuke

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-import Splash from '../components/splash.js'
-import Steps from '../components/steps.js'
-import Info from '../components/info.js'
-import Details from '../components/details.js'
-
 const Home = () => {
   return (
     <div className="g__container">
-      <Splash />
-      <Steps />
-      <Info />
-      <Details />
     </div>
   );
 };
