ceb0e3bcd476d5d3039369a7f7611b4309ee2a1a
Max Jowett <jowett.max@gmail.com>
2020-10-24 20:40:44 -0700

    Nuke x2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import styles from '../styles/Home.module.css';
 
 const Home = () => {
   return (
-    <div className="g__container">
+    <div>
+      <h1>Peer to peer video chat should be simple</h1>
     </div>
   );
 };
