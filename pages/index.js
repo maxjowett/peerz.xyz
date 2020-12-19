7fbda82d34819738f5ef239b9c49798d0f7677a7
Max Jowett <jowett.max@gmail.com>
2020-12-19 13:56:47 -0800

    Render splash section

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,21 @@ import styles from '../styles/Home.module.scss';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Peer to peer video chat should be simple</h1>
+    <div className={styles.top}>
+      <div className={styles.splash}>
+        <div className={styles.splash__left}>
+          <h1>
+            Peer to peer
+            <br />
+            video chat
+            <br />
+            should be simple
+          </h1>
+        </div>
+        <div className={styles.splash__right}>
+          <img className={styles.splash__img} src="/blob.png" />
+        </div>
+      </div>
     </div>
   );
 };
